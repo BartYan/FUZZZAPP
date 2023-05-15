314a3f1c17d05a73cc49f6d30dcc6d5004fce62c
Bartosz Janusz <bartosz.yan@gmail.com>
2023-05-15 19:46:47 +0200

    fix(home): cards rwd

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ const Home = () => {
         <Explorer/>
         <Hero/>
         <About/>
-        {/* <Cards/> */}
+        <Cards/>
         <Newsletter/>
         <More/>
         {/* <Store/> */}
