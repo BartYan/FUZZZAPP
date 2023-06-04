6193bcdf430a5d005b802b17716e85abb616f8eb
Bartosz Janusz <bartosz.yan@gmail.com>
2023-06-04 20:56:27 +0200

    fix(ai): page content init

--- pages/chordzapp/chordzAiTeacher.js
+++ pages/chordzapp/chordzAiTeacher.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../../styles/Home.module.scss'
 
 import useLang from '../hooks/useLang';
+import NavShorts from '../../components/nav/nav-shorts';
 import AsideNav from '../../components/nav/asideNav';
 import Transition from '../../components/transition';
 
@@ -151,17 +152,41 @@ export default function ChordzAiTeacher() {
             </Head>
 
             <div className={styles.apps}>
+                <NavShorts/>
                 <AsideNav
                     title={useLang('chordzappAiMenuTitle')}
                     desc={useLang('chordzappAiMenuDesc')}
                 />
                 <div className={styles.apps__content}>
-                    Ai teacher
-                    {/* <form action="http://localhost:8080/api/predict" method="post">
-                        <input type="text"/>
-                        <input type="submit"/>
-                    </form> */}
-                    {/* <button onClick={() => testFunc()}>click me</button> */}
+                    <main className={styles.apps__main}>
+                        <div className={`${styles.baner} ${styles.baner__info}`}>
+                            <div className={styles.baner__wrapp}>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="48" fill="#000" viewBox="0 -960 960 960" width="48"><path d="M453-280h60v-240h-60v240Zm26.982-314q14.018 0 23.518-9.2T513-626q0-14.45-9.482-24.225-9.483-9.775-23.5-9.775-14.018 0-23.518 9.775T447-626q0 13.6 9.482 22.8 9.483 9.2 23.5 9.2Zm.284 514q-82.734 0-155.5-31.5t-127.266-86q-54.5-54.5-86-127.341Q80-397.681 80-480.5q0-82.819 31.5-155.659Q143-709 197.5-763t127.341-85.5Q397.681-880 480.5-880q82.819 0 155.659 31.5Q709-817 763-763t85.5 127Q880-563 880-480.266q0 82.734-31.5 155.5T763-197.684q-54 54.316-127 86Q563-80 480.266-80Zm.234-60Q622-140 721-239.5t99-241Q820-622 721.188-721 622.375-820 480-820q-141 0-240.5 98.812Q140-622.375 140-480q0 141 99.5 240.5t241 99.5Zm-.5-340Z"/></svg>
+                                <p className={styles.baner__desc}>
+                                    {/* {useLang('chordzappAiMenuDesc')} */}
+                                    Nullam id magna vitae arcu dapibus congue vel at diam. 
+                                    Curabitur fermentum velit orci, quis pharetra diam ullamcorper tempor. 
+                                    Suspendisse accumsan non felis venenatis elementum. 
+                                    Nullam id magna vitae arcu dapibus congue vel at diam.
+                                </p>
+                            </div>
+                        </div>
+                        <article className={styles.apps__main_article}>
+                            <h2 className={styles.apps__main_article_title}><span>01.</span> Major Seven</h2>
+                            <p className={styles.apps__main_article_desc}>
+                                Nullam id magna vitae arcu dapibus congue vel at diam. 
+                                Curabitur fermentum velit orci, quis pharetra diam ullamcorper tempor. 
+                                Suspendisse accumsan non felis venenatis elementum. 
+                                Nullam id magna vitae arcu dapibus congue vel at diam. 
+                            </p>
+                            <div>
+
+                            </div>
+                        </article>
+                        
+                    </main>
+                    <div className={styles.adds__aside}>
+                    </div>
                 </div>
             </div>
         </Transition>
